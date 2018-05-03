3ccb38112bb520a6f8b99f5638858da7735c788a
mzl001 <1276581914@qq.com>
2018-05-03 17:46:53 +0800

    init

--- dome-server/src/main/java/com/ugood/controller/TestController.java
+++ dome-server/src/main/java/com/ugood/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.ugood.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by mzl on 2018/5/3.
  */
 @RestController
+@RefreshScope
 public class TestController {
 
     @Value("${foo}")
